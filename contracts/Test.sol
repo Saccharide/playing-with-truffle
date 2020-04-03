b721346c974bc9c58e246da025b804d498ef98c4
saccharide <rtang@email.wm.edu>
2020-04-02 20:12:12 -0400

    added working testing

---
+++ contracts/Test.sol
@@ -0,0 +1,32 @@
+pragma solidity ^0.5.0;
+
+contract Test{
+
+
+    uint mask = 2;
+    
+    function problem2(uint nonce) public returns (bool){
+        bool result = false;
+        //address _address = parseAddr("0xa7177777f7563fab5c8e4c98a4fb7a21416be12d");
+        address _address = 0xa7177777f7563Fab5C8E4c98a4fB7A21416be12d;
+
+        uint hash = uint(sha256(abi.encode(nonce, _address)));
+        
+        mask *= 2;
+        if (hash % 4 == 0){
+            result = true;
+        }
+        return result;
+    }
+    
+    function testp2() public returns (uint) {
+        uint ans = 0;
+        for(uint i = 700; i < 900; i++) {
+            if (problem2(i)) {
+                ans = i;
+                break;
+            }
+        }
+        return ans;
+    }
+}
--- public/bundle.js
+++ public/bundle.js
@@ -1,110 +1,30 @@
 var contractABI = [
     {
+      "constant": false,
       "inputs": [
         {
-          "internalType": "bytes32[]",
-          "name": "_candidateList",
-          "type": "bytes32[]"
+          "internalType": "uint256",
+          "name": "nonce",
+          "type": "uint256"
         }
       ],
-      "payable": false,
-      "stateMutability": "nonpayable",
-      "type": "constructor"
-    },
-    {
-      "constant": true,
-      "inputs": [],
-      "name": "creator",
+      "name": "problem2",
       "outputs": [
         {
-          "internalType": "address",
+          "internalType": "bool",
           "name": "",
-          "type": "address"
+          "type": "bool"
         }
       ],
       "payable": false,
-      "stateMutability": "view",
+      "stateMutability": "nonpayable",
       "type": "function"
     },
     {
-      "constant": true,
-      "inputs": [
-        {
-          "internalType": "bytes32",
-          "name": "str",
-          "type": "bytes32"
-        }
-      ],
-      "name": "bytes32ToString",
+      "constant": false,
+      "inputs": [],
+      "name": "testp2",
       "outputs": [
-        {
-          "internalType": "string",
-          "name": "",
-          "type": "string"
-        }
-      ],
-      "payable": false,
-      "stateMutability": "pure",
-      "type": "function"
-    },
-    {
-      "constant": false,
-      "inputs": [
-        {
-          "internalType": "bytes32",
-          "name": "_candidate",
-          "type": "bytes32"
-        }
-      ],
-      "name": "vote",
-      "outputs": [],
-      "payable": false,
-      "stateMutability": "nonpayable",
-      "type": "function"
-    },
-    {
-      "constant": false,
-      "inputs": [
-        {
-          "internalType": "string",
-          "name": "_candidate",
-          "type": "string"
-        }
-      ],
-      "name": "vote",
-      "outputs": [],
-      "payable": false,
-      "stateMutability": "nonpayable",
-      "type": "function"
-    },
-    {
-      "constant": false,
-      "inputs": [],
-      "name": "closeVote",
-      "outputs": [],
-      "payable": false,
-      "stateMutability": "nonpayable",
-      "type": "function"
-    },
-    {
-      "constant": false,
-      "inputs": [],
-      "name": "reopenVote",
-      "outputs": [],
-      "payable": false,
-      "stateMutability": "nonpayable",
-      "type": "function"
-    },
-    {
-      "constant": true,
-      "inputs": [],
-      "name": "getWinner",
-      "outputs": [
-        {
-          "internalType": "string",
-          "name": "",
-          "type": "string"
-        },
         {
           "internalType": "uint256",
           "name": "",
@@ -112,57 +32,36 @@ var contractABI = [
         }
       ],
       "payable": false,
-      "stateMutability": "view",
+      "stateMutability": "nonpayable",
       "type": "function"
     }
-  ];
-const contractAddress = '0x732bc1a64ad6dC0e1648B498105C01FCE1695483';
+];
+const contractAddress = '0xc48C09B54AF1bC06A2B7fEC561C8f8ad691B34b5';
+
+
 const web3 = new Web3('http://localhost:9545');
-const votingContract= new web3.eth.Contract(contractABI, contractAddress);
+const smartContract= new web3.eth.Contract(contractABI, contractAddress);
 
-//console.log(votingContract)
-//
-//web3.eth.getAccounts()
-//.then(console.log);
 
-//votingContract.methods.vote("candidate 1").call().then(() => {
-//    console.log(votingContract.winner);
-//    console.log(votingContract.max_vote);
-//    console.log("finished voting");
-//});
-//votingContract.methods.closeVote().call().then((_winner, _votes) => {
-//            console.log(_winner);
-//            console.log(_votes);
-//        });
 document.addEventListener('DOMContentLoaded', () => {
-    const $candidateName = document.getElementById('candidate-name');
-    const $voteButton = document.getElementById("vote");
-    const $winner = document.getElementById('winner');
-    const $votes  = document.getElementById('max-votes');
+    const $runButton = document.getElementById("run");
+    const $nounce  = document.getElementById('nounce-result');
 
-    let accounts = [];
-    // populate test accounts
-    web3.eth.getAccounts()
-        .then(_accounts => {
-            accounts = _accounts;
-        });
-
-    //
-    const getWinner = () => {
-        votingContract.methods.closeVote().call().then((_winner, _votes) => {
-            console.log(_winner);
-            console.log(_votes);
-            $winner.innerHTML = _winner;
-            $votes.innerHTML = _votes;
-        });
-    };
-
-    getWinner();
-    $voteButton.addEventListener('submit', e => {
+    // normal works
+    console.log("STARTED");
+    smartContract.methods.testp2().call().then(_result => {
+        $nounce.innerHTML = _result;
+        console.log("result = ", _result);
+    });
+ 
+    // Prevent default not working.....
+    $runButton.addEventListener('submit', e => {
         e.preventDefault();
-        const name = e.target.elements[0].value;
-        console.log("name entered : "+name);
-        votingContract.methods.vote(name).send({from: accounts[0]}).then(getWinner);
+        console.log("Running test : ");
+        smartContract.methods.testp2().call().then(_result => {
+            $nounce.innerHTML = _result;
+            console.log("result = ", _result);
+        });
     });
 
 });
